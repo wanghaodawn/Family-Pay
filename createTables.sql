be4ad419f0946c0134659c4981e0a5c9249cc839
Dawn <wanghaodawn@cmu.edu>
2017-05-04 18:36:51 -0400

    Added model and helper

--- createTables.sql
+++ createTables.sql
@@ -1,17 +1,16 @@
 CREATE TABLE IF NOT EXISTS users (
     user_id INT NOT NULL AUTO_INCREMENT,
     username VARCHAR(20) NOT NULL,
-    password VARCHAR(20) NOT NULL,
     user_type VARCHAR(20),
     PRIMARY KEY(user_id)
 );
 
 CREATE TABLE IF NOT EXISTS relationships (
-    child_username VARCHAR(20) NOT NULL,
-    parent_username VARCHAR(20) NOT NULL,
+    child_user_id VARCHAR(20) NOT NULL,
+    parent_user_id VARCHAR(20) NOT NULL,
     one_time_quota DOUBLE,
     monthly_quota DOUBLE,
-    PRIMARY KEY(parent_username, child_username),
-    FOREIGN KEY(child_username) REFERENCES users(username) ON DELETE CASCADE,
-    FOREIGN KEY(parent_username) REFERENCES users(username) ON DELETE CASCADE
+    PRIMARY KEY(parent_user_id, child_user_id),
+    FOREIGN KEY(child_user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+    FOREIGN KEY(parent_user_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
--- helper.js
+++ helper.js
@@ -8,8 +8,20 @@ module.exports = {
     FAIL: 'Database Failure',
     MISSING_USERNAME: 'Missing Username',
     MISSING_USER_TYPE: 'Missing user_type',
+    MISSING_PASSWORD: 'Missing Password',
+    MISSING_IMAGE: 'Missing Image',
     INVALID_USER_TYPE: 'Invalid user_type',
-    READ_IMAGE_ERROR: 'Cannot read the image'
+    READ_IMAGE_ERROR: 'Cannot read the image',
+    NO_FACEPP_API_KEY_FOUNT: 'Cannot get the api key of face++',
 
 
+    getFacePPAPIKey : function (callback) {
+        fs.readFile('FACEPP_API_KEY.dat', 'utf8', function (err, data) {
+          if (err) {
+                console.log(err);
+                return callback('Cannot find the API Key of Face++');
+            }
+            return callback(data);
+        });
+    }
 }
